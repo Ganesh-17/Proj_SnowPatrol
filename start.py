23dcd534dcf9aed4144c6458c62ea5c73310c0ff
Ganesh-17 <120444421+Ganesh-17@users.noreply.github.com>
2023-09-28 11:21:56 +0530

    Update start.py

--- start.py
+++ start.py
@@ -12,8 +12,8 @@ project_home = Path(find_dotenv()).parent
 sys.path.append(str(project_home))
 
 # # Set the page layout to be wide (call this only once, at the beginning)
-# st.set_page_config(layout="wide",
-#                    initial_sidebar_state="collapsed")
+st.set_page_config(layout="wide",
+                   initial_sidebar_state="collapsed")
 
 def build_snowpark_session(kwargs) -> Session:
     try:
